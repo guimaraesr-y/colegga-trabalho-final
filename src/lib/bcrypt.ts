3da1976e4fc32859f9d90369d4ae911c78098658
Ryan Guimarães <guimaraesryan26@gmail.com>
2024-12-24 19:12:32 -0300

    Fix bcrypt logic

--- src/lib/bcrypt.ts
+++ src/lib/bcrypt.ts
@@ -4,14 +4,13 @@ export default class BCrypt {
     
     private static saltRounds = 11;
 
-    static async hash(password: string): Promise<string> {
+    static hash(password: string) {
         const salt = bcrypt.genSaltSync(this.saltRounds);
         return bcrypt.hashSync(password, salt);
     }
 
-    static async compare(password: string, hash: string) {
-        const result = bcrypt.compareSync(password, hash);
-        return result;
+    static compare(password: string, hash: string) {
+        return bcrypt.compareSync(password, hash);
     }
     
 }

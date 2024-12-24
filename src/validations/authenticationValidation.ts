c2a3e89fb3b9d8fa21dfd7414869dc3e3b8845b0
Eduardo <eduardoschiavo70@gmail.com>
2024-12-23 22:14:00 -0300

    starting frontend of authentication form

---
+++ src/validations/authenticationValidation.ts
@@ -0,0 +1,33 @@
+"use client"
+
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+
+const formSchema = z.object({
+  email: z.string().email( {
+    message: "Email inválido",
+  }),
+  password: z.string().min(8, {
+    message: "Senha deve ter pelo menos 8 caractéres"
+  })
+})
+
+
+export function ProfileForm() {
+  // 1. Define your form.
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  })
+
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values)
+  }
+}
